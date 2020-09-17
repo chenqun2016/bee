6174ba07fccbc1f7d235922b5287fefd2f1f1d5b
chenqun <1715666965@qq.com>
2020-09-17 22:19:25 +0800

    pay status activity

--- app/src/main/java/com/bee/user/ui/order/PayStatusActivity.java
+++ app/src/main/java/com/bee/user/ui/order/PayStatusActivity.java
@@ -2,11 +2,14 @@ package com.bee.user.ui.order;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bee.user.R;
 import com.bee.user.ui.MainActivity;
 import com.bee.user.ui.base.activity.BaseActivity;
 
+import butterknife.BindView;
 import butterknife.OnClick;
 
 /**
@@ -15,6 +18,25 @@ import butterknife.OnClick;
  * 描述：
  */
 public class PayStatusActivity extends BaseActivity {
+    @BindView(R.id.iv_icon)
+    ImageView iv_icon;
+
+    @BindView(R.id.pay_status)
+    TextView pay_status;
+
+    @BindView(R.id.des)
+    TextView des;
+
+    @BindView(R.id.des2)
+    TextView des2;
+
+    @BindView(R.id.btn_1)
+    TextView btn_1;
+
+    @BindView(R.id.btn_2)
+    TextView btn_2;
+
+
     @OnClick({R.id.btn_1,R.id.btn_2})
     public void onClick(View view){
         switch (view.getId()){
